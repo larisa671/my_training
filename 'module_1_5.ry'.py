c5b2235d9562440b94f882d6110ac1058db24d45
larisa671 <kim-sml@rambler.ru>
2024-08-18 20:32:47 +0300

    Add files via upload

---
+++ 'module_1_5.ry'.py
@@ -0,0 +1,23 @@
+# tuple_ = 1, 2, 3, True, 'String'
+# uple_2 = (1, 2, 3, 4)
+# ple_3 = tuple([1, 2, 3, 4])
+# print(type(tuple_))
+# print(tuple_)
+# print(tuple_2)
+# print(tuple_3)
+# print(tuple_[0])
+# tuple_ = ([1, 2], 0) + (1, 2)
+# print(tuple_)
+# tuple_[0][0] = 2
+# print(tuple_)
+# tuple_ = (1, 2)*3
+# print(tupl_)
+immutable_var = 1, 2, 3, True, 'Strong'
+print(immutable_var)
+print(immutable_var[0])
+mutable_list = [1, 2, 3, True, 'String']
+print(mutable_list)
+mutable_list[0]= '1'
+print(mutable_list)
+
+
